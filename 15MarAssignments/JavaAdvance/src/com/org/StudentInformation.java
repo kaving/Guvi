f6f50ef1994cec0bc2d1b6ce4382dc3692bfc35b
kaving <kavin609@gmail.com>
2025-03-23 09:23:08 +0530

    Added files

--- 15MarAssignments/JavaAdvance/src/com/org/StudentInformation.java
+++ 15MarAssignments/JavaAdvance/src/com/org/StudentInformation.java
@@ -5,12 +5,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StudentInformation {
-    /**
-     * There are 10 students decided to give special gifts to those who name start with A.
-     * Java program to separate those, who name start with A.
-     * List interface to store the names of the students.
-     * lambda expression and Stream API to filter the names.
-     */
     public static void main(String[] args) {
         List<String> students = Arrays.asList("Amit", "Arun", "John", "Anil", "Ankit", "Aman", "Rahul", "Alok", "Ankur", "Rohit");
         List<String> studentsWithA = students.stream().filter(s -> s.startsWith("A")).collect(Collectors.toList());
