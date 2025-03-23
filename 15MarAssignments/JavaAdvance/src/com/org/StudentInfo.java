0ead2fb392d00a7708212ef3d8eb708f2f412305
kaving <kavin609@gmail.com>
2025-03-23 09:25:24 +0530

    Added file

--- 15MarAssignments/JavaAdvance/src/com/org/StudentInfo.java
+++ 15MarAssignments/JavaAdvance/src/com/org/StudentInfo.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class StudentInformation {
+public class StudentInfo {
     public static void main(String[] args) {
         List<String> students = Arrays.asList("Amit", "Arun", "John", "Anil", "Ankit", "Aman", "Rahul", "Alok", "Ankur", "Rohit");
         List<String> studentsWithA = students.stream().filter(s -> s.startsWith("A")).collect(Collectors.toList());
